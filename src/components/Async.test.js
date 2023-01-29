c95f7e82772946a00d5b6acdaaa795c9135b3cd3
naekit <mykyta.onishchuck@code.berlin>
2023-01-29 17:25:19 -0500

    mock test function

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -4,6 +4,15 @@ import Async from "./Async"
 describe("Async component", () => {
 	test("renders posts if request succeeds", async () => {
 		// Arrange
+		window.fetch = jest.fn()
+		window.fetch.mockResolvedValueOnce({
+			json: async () => [
+				{
+					id: "p1",
+					title: "First post",
+				},
+			],
+		})
 		render(<Async />)
 		// Act
 		// ...nothing to do here
