86d4a49dcf1b3f2f3df9224666e25ae94ad3a306
naekit <mykyta.onishchuck@code.berlin>
2023-01-29 17:12:35 -0500

    userevent testing for change in state

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import Greeting from "./Greeting"
 
 describe("Greeting component", () => {
@@ -10,5 +11,41 @@ describe("Greeting component", () => {
 		// Assert
 		const greetingElement = screen.getByText("Hello, world!")
 		expect(greetingElement).toBeInTheDocument()
+	})
+
+	test("initial component has unchanged text", () => {
+		// Arrange
+		render(<Greeting />)
+		// Act
+		// ...nothing to do here
+		// Assert
+		const outputElement = screen.getByText(
+			"Greetings to all that are in it!"
+		)
+		expect(outputElement).toBeInTheDocument()
+	})
+
+	test("text changes after button click", () => {
+		// Arrange
+		render(<Greeting />)
+		// Act
+		const buttonElement = screen.getByRole("button")
+		userEvent.click(buttonElement)
+		// Assert
+		const outputElement = screen.getByText("Changed!")
+		expect(outputElement).toBeInTheDocument()
+	})
+
+	test("does not render greeting if button is clicked", () => {
+		// Arrange
+		render(<Greeting />)
+		// Act
+		const buttonElement = screen.getByRole("button")
+		userEvent.click(buttonElement)
+		// Assert
+		const outputElement = screen.queryByText(
+			"Greetings to all that are in it!"
+		)
+		expect(outputElement).toBeNull()
 	})
 })
